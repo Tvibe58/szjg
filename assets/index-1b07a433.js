65daa14df3e824d8174b87b6edcbd5781d42f545
Tvibe <tvibe58@163.com>
2024-11-25 16:36:34 +0800

    update

---
+++ assets/index-1b07a433.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-d576a5ae.js";import{u as r}from"./useTranslation-a842998b.js";const n=()=>{const{t:s}=r();return t.jsxs("div",{children:[s("test")," ",s("menus.关于")]})};export{n as default};
