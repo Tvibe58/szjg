0720790e1a3c998ece184830972d484cb4ff0e86
Tvibe <tvibe58@163.com>
2024-11-27 10:29:38 +0800

    test

---
+++ assets/index-26942050.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-0b302016.js";import{u as r}from"./useTranslation-928c11ca.js";const n=()=>{const{t:s}=r();return t.jsxs("div",{children:[s("test")," ",s("menus.关于")]})};export{n as default};
